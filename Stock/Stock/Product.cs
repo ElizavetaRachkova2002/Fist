c9e65d5d25adf4d16285b6395a90f0e67b9ee783
Elizaveta Rachkova <rachkovb@bk.ru>
2022-10-02 09:37:15 +0300

    Рачковой

---
+++ Stock/Stock/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stock
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public string Legal_entity { get; set; }
+        public int Barcode { get; set; }
+        public int Count { get; set; }
+
+        public int Packed { get; set; }
+        public int Not_packed { get; set; }
+    }
+}
---
+++ Stock/Stock/ProductsAndPackage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stock
+{
+     class ProductsAndPackage
+    {
+       
+        class Packaging
+        {
+            public string Name { get; set; }
+            public string Size { get; set; }
+            public int Count { get; set; }
+        }
+    }
+}
